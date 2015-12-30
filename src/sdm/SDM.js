57f56546b0fe6d7f619b3a88a546fd095698cc27
John Tigue <john@tigue.com>
2015-12-30 00:38:08 -0800

    Added docs

--- src/sdm/SDM.js
+++ src/sdm/SDM.js
@@ -1,3 +1,7 @@
+/* Main object type.
+ * Parameters:
+ *   size: # of "neurons" defaults to 10,000 as that is the commonly accepted place to start in order for the math to work
+ */
 var SDM = (size = 10000) => {
-  
+  this.size = size;
   }
