5fdcace280fa02fbfb90d57b7d59b4476815e217
John Tigue <john@tigue.com>
2016-01-06 01:39:38 -0800

    Notes.

--- src/sdm/SDM.js
+++ src/sdm/SDM.js
@@ -2,6 +2,17 @@
  * Parameters:
  *   size: # of "neurons" defaults to 10,000 as that is the commonly accepted place to start in order for the math to work
  */
+
+/* ML libs:
+ * http://www.datasciencecentral.com/profiles/blogs/machine-learning-in-javascript-a-compilation-of-resources
+ * http://www.datascienceweekly.org/data-scientist-interviews/training-deep-learning-models-browser-andrej-karpathy-interview
+ */
+
+/* GPU libs
+ * https://github.com/kashif/node-cuda
+ * http://stack.gl/
+ *   via http://stackoverflow.com/questions/15213216/accessing-gpu-via-web-browser
+ */
 var SDM = (size = 10000) => {
   this.size = size;
   }
