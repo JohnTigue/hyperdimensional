dce7e4c6964d074352749a8bf356d59121a4d444
John Tigue <john@tigue.com>
2016-01-07 15:03:44 -0800

    notes.

--- src/sdm/SDM.js
+++ src/sdm/SDM.js
@@ -3,12 +3,15 @@
  *   size: # of "neurons" defaults to 10,000 as that is the commonly accepted place to start in order for the math to work
  */
 
-/* ML libs:
+/* While-designing notes:
+ * The goal here is to see if JS can be used for neural nets without sacrificing performance.
+ * Can various tricks be used to get the hardcore math to be done for JS via the GPU, or can something such as asm.js do the trick?
+ *
+ * ML libs:
  * http://www.datasciencecentral.com/profiles/blogs/machine-learning-in-javascript-a-compilation-of-resources
  * http://www.datascienceweekly.org/data-scientist-interviews/training-deep-learning-models-browser-andrej-karpathy-interview
- */
-
-/* GPU libs
+ *
+ * GPU libs:
  * https://github.com/kashif/node-cuda
  * http://stack.gl/
  *   via http://stackoverflow.com/questions/15213216/accessing-gpu-via-web-browser
